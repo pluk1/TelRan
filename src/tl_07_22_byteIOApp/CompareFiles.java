f56c722cea6eef99715cc8df2ce629f88f4eed84
cartman <9225534@gmail.com>
2018-07-25 07:32:16 +0300

    comment not needed

--- src/tl_07_22_byteIOApp/CompareFiles.java
+++ src/tl_07_22_byteIOApp/CompareFiles.java
@@ -9,29 +9,46 @@ public class CompareFiles {
         String file1 = "/Users/cartman/Documents/text1.txt";
         String file2 = "/Users/cartman/Documents/text2.txt";
 
-//        FileInputStream flInput1 = new FileInputStream(file1);
-//        FileInputStream flInput2 = new FileInputStream(file2);
+        FileInputStream flInput1 = new FileInputStream(file1);
+        FileInputStream flInput2 = new FileInputStream(file2);
 
-        ArrayList<Integer> arr1 = readFile(file1);
-        ArrayList<Integer> arr2 = readFile(file2);
+//        ArrayList<Integer> arr1 = readFile(file1);
+//        ArrayList<Integer> arr2 = readFile(file2);
 
-        System.out.println(arr1.equals(arr2));
+        int size1 = flInput1.available();
+        int size2 = flInput2.available();
+        boolean result = true;
 
+        if (size1==size2){
+            for (int i = 0; i <= size1; i++) {
 
-    }
-
-    static ArrayList<Integer> readFile(String filePath) throws IOException {
-
-        ArrayList<Integer> arr = new ArrayList<>();
-        FileInputStream flInput = new FileInputStream(filePath);
-
-        int size = flInput.available();
-        for (int i = 0; i <= size; i++) {
-            arr.add(flInput.read());
+                if (flInput1.read() != flInput2.read()){
+                    result = false;
+                    break;
+                }
+            }
+        } else {
+            result = false;
         }
 
-        flInput.close();
 
-        return arr;
+        System.out.println("Are files equals?: " + result);
+
+
     }
+
+//    static ArrayList<Integer> readFile(String filePath) throws IOException {
+//
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        FileInputStream flInput = new FileInputStream(filePath);
+//
+//        int size = flInput.available();
+//        for (int i = 0; i <= size; i++) {
+//            arr.add(flInput.read());
+//        }
+//
+//        flInput.close();
+//
+//        return arr;
+//    }
 }
